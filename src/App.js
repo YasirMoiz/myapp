7e6515be45bbdfb170f420463aa407aa71be6123
Yasir-22 <yasirmoiz01@gmail.com>
2020-12-17 00:45:03 +0500

    again change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Child from './child'
 
 function App() {
   return (
     <div className="App">
-    <h1>HEllo World</h1>
+    <h1>You Are in Xyz Resturant</h1>
+     <Child  dish="Biryan" sweetdish="halwa"/>
+     <hr />
+     <Child  dish="Paya" sweetdish="Jalebi"/>
+     <hr />
+
+     <Child  dish="Nirhari" sweetdish="lab e shree"/>
+     <hr />
+
+
     </div>
   );
 }
---
+++ src/child.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Child() {
+    return (
+        <div >
+        <h1>Today name is {props.dist} and {props.sweetdish} </h1>
+
+        </div>
+        
+        
+    );
+  }
+  
+
+ export default Child; 
